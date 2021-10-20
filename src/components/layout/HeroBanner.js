5a610e7403f5113e7e00e4d3abac632e860f595b
Shamil Noroff <shamil_b93@hotmail.com>
2021-10-21 00:31:32 +0200

    fifth commit

--- src/components/layout/HeroBanner.js
+++ src/components/layout/HeroBanner.js
@@ -4,30 +4,28 @@ import WebIcons from "./WebIcons";
 function HeroBanner() {
   return (
     <>
-      <section className="hero">
-        <div className="wrapper">
-          <div className="presentation">
-            <div className="information">
-              <h1>Shamil Babujev</h1>
-              <h2>[ Front End Developer, Webdesigner, Digital Marketer ]</h2>
-              <div className="web-icons">
-                <WebIcons />
-              </div>
-              <p>Junior Developer looking for a job. In the meantime I work on my own projects that I upload to the site. </p>
+      <div className="wrapper">
+        <div className="presentation">
+          <div className="information">
+            <h1>Shamil Babujev</h1>
+            <h2>[ Front End Developer, Webdesigner, Digital Marketer ]</h2>
+            <div className="web-icons">
+              <WebIcons />
+            </div>
+            <p>Junior Developer looking for a job. In the meantime I work on my own projects that I upload to the site. </p>
 
-              <div className="buttons">
-                <a href="#footer" className="contact-btn">
-                  Contact
-                </a>
-                <a href="#projects" className="portfolio-btn">
-                  Portfolio
-                </a>
-              </div>
+            <div className="buttons">
+              <a href="#footer" className="contact-btn">
+                Contact
+              </a>
+              <a href="#projects" className="portfolio-btn">
+                Portfolio
+              </a>
             </div>
           </div>
-          <div style={{ backgroundImage: "url(/assets/img/shamil28.jpg)" }} className="portrait-pic"></div>
         </div>
-      </section>
+        <div style={{ backgroundImage: "url(/assets/img/shamil28.jpg)" }} className="portrait-pic"></div>
+      </div>
     </>
   );
 }
